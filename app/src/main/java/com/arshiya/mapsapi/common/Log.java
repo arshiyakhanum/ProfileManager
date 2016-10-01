d136121b96ee2728d5daa3d666c69e201640903f
arshiya <arshiya>
2016-10-02 01:06:41 +0530

    added logger class

---
+++ app/src/main/java/com/arshiya/mapsapi/common/Log.java
@@ -0,0 +1,16 @@
+package com.arshiya.mapsapi.common;
+
+/**
+ * @author Arshiya Khanum on 10/1/2016.
+ */
+public class Log {
+
+  private final int V = 0;
+  private final int D = 1;
+  private final int I = 2;
+  private final int W = 3;
+  private final int E = 4;
+  private final int A = 5;
+
+
+}
